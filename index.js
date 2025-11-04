58b9e05ef760bd434a66340650f36494ed362a14
nataliayatsishin <nata.yatsishin@gmail.com>
2025-11-03 19:17:24 -0600

    add template literals

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 const firstName = "Nataliia";
 const age = 25;
-console.log(`Hi! My name is ` + firstName + ` and I am ` + age + `years old!`);
+console.log(`Hi! My name is ` + firstName + ` and I am ` + age + ` years old!`);
+console.log(`Hi! My name is ${firstName} and I am ${age} years old!`);
